fad891d0d439529d5f352a06211f145a27f05e02
Junggu <justin403502@naver.com>
2022-04-07 17:42:17 +0900

    add speed modification

--- src/lane_control.py
+++ src/lane_control.py
@@ -18,6 +18,7 @@ class PID_control:
         self.Ki = ki
         self.Kd = kd
         self.speed = speed
+        self.absoluteSpeed = speed
         self.p_error = 0.0
         self.i_error = 0.0
         self.d_error = 0.0
@@ -28,7 +29,7 @@ class PID_control:
         Angle = self.pid_control(cte)
         msg = xycar_motor()
         msg.angle = Angle
-        msg.speed = self.speed
+        msg.speed = self.absoluteSpeed if abs(Angle)>30 else 5
         self.pub.publish(msg)
 
     def pid_control(self, error):
@@ -38,28 +39,28 @@ class PID_control:
 
         return self.Kp*self.p_error + self.Ki*self.i_error + self.Kd*self.d_error
 
-class Pure_pursuit:
+# class Pure_pursuit:
 
-    def __init__(self, lfd, x_ratio, y_ratio):
+#     def __init__(self, lfd, x_ratio, y_ratio):
 
-        self.lfd = lfd
-        self.x_ratio = x_ratio
-        self.y_ratio = y_ratio
+#         self.lfd = lfd
+#         self.x_ratio = x_ratio
+#         self.y_ratio = y_ratio
 
-        self.pub = rospy.Publisher('xycar_motor', xycar_motor, queue_size=1)
+#         self.pub = rospy.Publisher('xycar_motor', xycar_motor, queue_size=1)
 
-    def drive(self, cte, Speed):
+#     def drive(self, cte, Speed):
 
-        x = cte/self.x_ratio
-        y = 240/self.y_ratio
+#         x = cte/self.x_ratio
+#         y = 240/self.y_ratio
 
-        theta = atan2(x, y)
-        Angle = -atan2((2*sin(theta)), self.lfd)
+#         theta = atan2(x, y)
+#         Angle = -atan2((2*sin(theta)), self.lfd)
 
-        msg = xycar_motor()
-        msg.angle = Angle
-        msg.speed = Speed
-        self.pub.publish(msg)
+#         msg = xycar_motor()
+#         msg.angle = Angle
+#         msg.speed = Speed
+#         self.pub.publish(msg)
 
         
 
